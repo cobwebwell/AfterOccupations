996fd627abfefb5e71a1179be6b4c4d572bd28bc
Administrator <Administrator@Administrator>
2025-08-20 12:38:32 +0300

    V6

--- Contents/mods/AfterOccupations/media/lua/shared/NPCs/AfterOccupations.lua
+++ Contents/mods/AfterOccupations/media/lua/shared/NPCs/AfterOccupations.lua
@@ -6,26 +6,137 @@ ProfessionFramework.RemoveDefaultProfessions = true
 
 -- set this to true to enable custom starting items.
 ProfessionFramework.AlwaysUseStartingKits = true
-
-function EditProfUnemployed()
-   local unemployed = ProfessionFactory.addProfession("unemployed", getText("UI_prof_unemployed"), "profession_unemployed", 0);
-   local unemployed = TraitFactory.addFreeTrait("kabus", getText("UI_trait_kabus"), "profession_unemployed");
    
-   getProfession('unemployed', {
-      clothing = { 
-           Boilersuit = {"Base.Boilersuit_Prisoner"},
-      },
-   })
-   
-end
+addProfession('common', {
+    name = "UI_prof_common",
+    icon = "prof_common",
+    cost = 0,
+    xp = {
+        [Perks.Sprinting] = 1,
+        [Perks.Strength] = 1,
+        [Perks.Fitness] = 1,
+        [Perks.Aiming] = 1,
+        [Perks.Blunt] = 1,
+        [Perks.SmallBlade] = 1,
+    },
+    clothing = {
+         Boilersuit = {"Base.Boilersuit_Prisoner"},
+    },
+})
 
-Events.OnGameBoot.Add(EditProfUnemployed);
-Events.OnPlayerDeath.Add(EditProfUnemployed);
-Events.OnCreatePlayer.Add(EditProfUnemployed);
-Events.OnCreateSurvivor.Add(EditProfUnemployed);
-Events.OnNewGame.Add(EditProfUnemployed);
-Events.OnPreGameStart.Add(EditProfUnemployed);
-Events.OnGameStart.Add(EditProfUnemployed);
-Events.OnCreateLivingCharacter.Add(EditProfUnemployed);
-Events.OnLoginState.Add(EditProfUnemployed);
-Events.OnPreMapLoad.Add(EditProfUnemployed);
+addProfession('sharpshooter', {
+    name = "UI_prof_sharpshooter",
+    icon = "prof_sharpshooter",
+    cost = -4,
+    xp = {
+        [Perks.Strength] = 2,
+        [Perks.Aiming] = 5,
+        [Perks.Reloading] = 3,
+        [Perks.Blunt] = 2,
+        [Perks.SmallBlunt] = 2,
+        [Perks.SmallBlade] = 1,
+    },
+    clothing = {
+         Boilersuit = {"Base.Boilersuit_Prisoner"},
+    },
+})
+
+addProfession('medic', {
+    name = "UI_prof_medic",
+    icon = "prof_medic",
+    cost = -2,
+    xp = {
+        [Perks.Doctor] = 5,
+        [Perks.SmallBlade] = 3,
+        [Perks.Lightfoot] = 2,
+    },
+    clothing = {
+         Boilersuit = {"Base.Boilersuit_Prisoner"},
+    },
+})
+
+addProfession('cook', {
+    name = "UI_prof_cook",
+    icon = "prof_cook",
+    cost = -2,
+    xp = {
+        [Perks.Cooking] = 5,
+        [Perks.Trapping] = 1,
+        [Perks.Farming] = 2,
+        [Perks.Fishing] = 1,
+        [Perks.SmallBlade] = 2,
+    },
+    recipes = {"Make Cake Batter", "Make Pie Dough", "Make Bread Dough"},
+    traits = {"Cook2"},
+    clothing = {
+         Boilersuit = {"Base.Boilersuit_Prisoner"},
+    },
+})
+
+addProfession('farmer', {
+    name = "UI_prof_farmer",
+    icon = "prof_farmer",
+    cost = -4,
+    xp = {
+        [Perks.Trapping] = 2,
+        [Perks.Farming] = 5,
+        [Perks.Fishing] = 2,
+	    [Perks.PlantScavenging] = 2,
+        [Perks.SmallBlade] = 2,
+    },
+    recipes = {"Make Cake Batter", "Make Pie Dough", "Make Bread Dough", "Make Fishing Rod", "Fix Fishing Rod", "Get Wire Back", "Make Fishing Net", "Make Mildew Cure", "Make Flies Cure"},
+    clothing = {
+         Boilersuit = {"Base.Boilersuit_Prisoner"},
+    },
+})
+
+
+addProfession('builder', {
+    name = "UI_prof_builder",
+    icon = "prof_builder",
+    cost = -4,
+    xp = {
+        [Perks.SmallBlunt] = 2,
+        [Perks.Woodwork] = 3,
+        [Perks.Strength] = 3,
+    },
+    clothing = {
+         Boilersuit = {"Base.Boilersuit_Prisoner"},
+    }
+})
+
+
+addProfession('thief', {
+    name = "UI_prof_thief",
+    icon = "prof_thief",
+    cost = -6,
+    xp = {
+        [Perks.Nimble] = 2,
+        [Perks.Sneak] = 2,
+        [Perks.Lightfoot] = 2,
+        [Perks.SmallBlunt] = 2,
+        [Perks.SmallBlade] = 4,
+    },
+    traits = {"Burglar"},
+    clothing = {
+         Boilersuit = {"Base.Boilersuit_Prisoner"},
+    },
+})
+
+addProfession('scrapmech', {
+    name = "UI_prof_scrapmech",
+    icon = "prof_scrapmech",
+    cost = -4,
+    xp = {
+        [Perks.Electricity] = 3,
+        [Perks.MetalWelding] = 4,
+        [Perks.SmallBlunt] = 4,
+        [Perks.Blunt] = 2,
+        [Perks.Mechanics] = 4,
+    },
+    clothing = {
+         Boilersuit = {"Base.Boilersuit_Prisoner"},
+    },
+    traits = {"Mechanics2"},
+    recipes = { "Basic Mechanics", "Intermediate Mechanics", "Advanced Mechanics", "Generator" }
+})
--- Contents/mods/AfterOccupations/media/lua/shared/NPCs/AfterTraits.lua
+++ Contents/mods/AfterOccupations/media/lua/shared/NPCs/AfterTraits.lua
@@ -3,11 +3,15 @@ require('NPCs/MainCreationMethods');
 local addTrait = ProfessionFramework.addTrait
 local getTrait = ProfessionFramework.getTrait
 
-addTrait("kabus", {
-    name = "UI_trait_kabus",
-    description = "UI_trait_kabusDesc",
-    profession = true,
-    cost = 0,
-})
+--addTrait('kabus', {
+--    name = "UI_trait_kabus",
+--    description = "UI_trait_kabusdesc",
+--    cost = 0,
+--    profession = true,
+--    xp = {
+--        [Perks.Nimble] = 1,
+--    },
+--})
+
 
 --REZERVE BEYLER
